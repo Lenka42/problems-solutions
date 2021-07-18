0f5c738eb6fa7f340f64b8cb40674ce23f83cce2
Elena Shylko <elena.shylko@gmail.com>
2021-07-18 23:16:13 +0300

    Hamming weight

--- others/hamming_weight.py
+++ others/hamming_weight.py
@@ -9,6 +9,15 @@ class SolutionMask:
         return bits
 
 
+class SolutionBitTrick:
+    def hammingWeight(self, n: int) -> int:
+        s = 0
+        while n != 0:
+            s += 1
+            n &= n - 1
+        return s
+
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count('1')
